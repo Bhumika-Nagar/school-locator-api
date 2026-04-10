d9df3e427023cb40b9c30b98482f575c8872a350
Bhumika nagar <nagarbhumika56@gmail.com>
2026-04-10 18:57:35 +0530

    again fixed index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 import dotenv from "dotenv";
 dotenv.config();
-import  {db}  from "./db.js";
-import express from "express";
 
+import { db } from "./db.js";
+import express from "express";
 import router from "./routes/schoolRoutes.js";
 
-const app= express();
-const PORT= process.env.PORT || 5000;
-app.use(express.json());
+const app = express();
 
+app.use(express.json());
 app.use("/api", router);
 
-app.use((err, req, res, next) => {
-  console.error(err); 
 
-    res.status(500).json({
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
     message: "internal server error",
-    error: err.message
+    error: err.message,
   });
 });
 
 async function startServer() {
-  await db(); 
+  await db();
 
-  app.listen(PORT, () => {
-    console.log("Server running");
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
 startServer();
-
-
