92dd70db72a49abd49c6514bfb35d06289229fbf
Bhumika nagar <nagarbhumika56@gmail.com>
2026-04-10 14:38:13 +0530

    server configuration

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 import mysql from "mysql2/promise";
 const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
