d0f03c14fbc8230a730553432d033ead461f996e
Bhumika nagar <nagarbhumika56@gmail.com>
2026-04-10 14:25:22 +0530

    start script

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from "express";
 import router from "./routes/schoolRoutes.js";
 
 const app= express();
-
+const PORT= process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api", router);
@@ -21,5 +21,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`server running on port ${process.env.PORT}`)
+    console.log(`server running on port ${PORT}`)
 });
