06531a76c4fc3ad87ab94df2557974e032025a84
Bhumika nagar <nagarbhumika56@gmail.com>
2026-04-10 15:03:19 +0530

    connected to mySQL

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from "express";
 import router from "./routes/schoolRoutes.js";
 
 const app= express();
-const PORT= process.env.PORT || 5000;
+const PORT= process.env.PORT;
 app.use(express.json());
 
 app.use("/api", router);
